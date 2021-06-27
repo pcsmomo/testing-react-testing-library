2cee1ed55d1d96bff6908d8c57e1777dbef3da1f
pcsmomo <dongwoo_333@hanmail.net>
2021-06-27 23:17:39 +1000

    12. Start Color Button App : Write tests first

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,15 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react button', () => {
+test('button has correct initial color', () => {
   render(<App />);
 
-  const buttonElement = screen.getByRole('button', { name: /Button Role/i });
-  expect(buttonElement).toBeInTheDocument();
+  // find an element with a role of button and text of 'Change of blue'
+  const colorButton = screen.getByRole('button', { name: /Change to blue/i });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
+
+test('button turns blue when clicked', () => {});
