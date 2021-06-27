d1d0c274301ff338f324437ce4e03c1444a6205a
pcsmomo <dongwoo_333@hanmail.net>
2021-06-28 09:13:23 +1000

    19. Code Quiz Solution: Confirm Button Disable on Checkbox Check

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
+
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
   return (
@@ -12,10 +14,16 @@ function App() {
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
