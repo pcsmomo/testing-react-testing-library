b535bdaafcf5dfb34c94da49ccd68525d4b914f7
pcsmomo <dongwoo_333@hanmail.net>
2021-06-28 09:48:39 +1000

    23. Code Quiz! Update Tests for New Color Names: Change tests first

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -5,27 +5,31 @@ import { replaceCamelWithSpaces } from './App';
 test('button has correct initial color', () => {
   render(<App />);
 
-  // find an element with a role of button and text of 'Change of blue'
-  const colorButton = screen.getByRole('button', { name: /Change to blue/i });
+  // find an element with a role of button and text of 'Change of Midnight Blue'
+  const colorButton = screen.getByRole('button', {
+    name: /Change to Midnight Blue/i
+  });
 
-  // expect the background color to be red
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+  // expect the background color to be Medium Violet Red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
 
   // click button
   fireEvent.click(colorButton);
 
-  // expect the background color to be blue
-  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  // expect the background color to be Midnight Blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' });
 
-  // expect the button text to be 'Changed to red'
-  expect(colorButton.textContent).toBe('Change to red');
+  // expect the button text to be 'Changed to Medium Violet Red'
+  expect(colorButton).toHaveTextContent('Change to Medium Violet Red');
 });
 
 test('initial conditions', () => {
   render(<App />);
 
   // check that the button starts out enabled
-  const colorButton = screen.getByRole('button', { name: /Change to blue/i });
+  const colorButton = screen.getByRole('button', {
+    name: /Change to Midnight Blue/i
+  });
   expect(colorButton).toBeEnabled();
 
   // check that the checkbox starts out unchecked
@@ -36,7 +40,9 @@ test('initial conditions', () => {
 test('Checkbox disables button on first click and enables on second click', () => {
   render(<App />);
   const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
-  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to Midnight Blue'
+  });
 
   fireEvent.click(checkbox);
   expect(colorButton).toBeDisabled();
@@ -45,10 +51,12 @@ test('Checkbox disables button on first click and enables on second click', () =
   expect(colorButton).toBeEnabled();
 });
 
-test('Disabled button has gray background and reverts to red', () => {
+test('Disabled button has gray background and reverts to Medium Violet Red', () => {
   render(<App />);
   const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
-  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to Midnight Blue'
+  });
 
   // disable button
   fireEvent.click(checkbox);
@@ -56,15 +64,17 @@ test('Disabled button has gray background and reverts to red', () => {
 
   // re-enable button
   fireEvent.click(checkbox);
-  expect(colorButton).toHaveStyle('background-color: red');
+  expect(colorButton).toHaveStyle('background-color: MediumVioletRed');
 });
 
-test('Clicked disable button has gray background and reverts to blue', () => {
+test('Clicked disabled button has gray background and reverts to Midnight Blue', () => {
   render(<App />);
   const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
-  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to Midnight Blue'
+  });
 
-  // change button to blue
+  // change button to Midnight Blue
   fireEvent.click(colorButton);
 
   // disable button
@@ -73,7 +83,7 @@ test('Clicked disable button has gray background and reverts to blue', () => {
 
   // re-enable button
   fireEvent.click(checkbox);
-  expect(colorButton).toHaveStyle('background-color: blue');
+  expect(colorButton).toHaveStyle('background-color: MidnightBlue');
 });
 
 describe('spaces before camel-case capital letters', () => {
@@ -83,7 +93,7 @@ describe('spaces before camel-case capital letters', () => {
   test('Works for one inner capital letter', () => {
     expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
   });
-  test('Works for multiple inner capital letter', () => {
+  test('Works for multiple inner capital letters', () => {
     expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
   });
 });
