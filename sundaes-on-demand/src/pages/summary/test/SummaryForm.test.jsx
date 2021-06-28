1c9106f26feba5da0e4516f3baaa1c1668289ac9
pcsmomo <dongwoo_333@hanmail.net>
2021-06-28 18:07:42 +1000

    40. Testing Element is Not on Page: Start Popover Tests

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -30,7 +30,25 @@ test('Checkbox enables button on first click and disables on second click', () =
 });
 
 test('popover responses to hover', () => {
+  render(<SummaryForm />);
+
   // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
   // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
   // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  const nullPopoverAgain = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopoverAgain).not.toBeInTheDocument();
 });
