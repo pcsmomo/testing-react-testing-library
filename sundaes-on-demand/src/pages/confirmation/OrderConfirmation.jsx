6ddb02e671d321cc3ffbf045bafe30b1ba02d84d
pcsmomo <dongwoo_333@hanmail.net>
2021-06-30 19:30:15 +1000

    83. Server Error on Order Confirmation Page

--- sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
+++ sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import { useOrderDetails } from '../../contexts/OrderDetails';
+import AlertBanner from '../common/AlertBanner';
 
 export default function OrderConfirmation({ setOrderPhase }) {
   const [, , resetOrder] = useOrderDetails();
   const [orderNumber, setOrderNumber] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
@@ -15,9 +17,13 @@ export default function OrderConfirmation({ setOrderPhase }) {
         setOrderNumber(response.data.orderNumber);
       })
       .catch((error) => {
-        // TODO: handle error here
+        setError(true);
       });
   }, []);
+
+  if (error) {
+    return <AlertBanner message={null} variant={null} />;
+  }
 
   function handleClick() {
     // clear the order details
