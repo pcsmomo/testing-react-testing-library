ccdddfef2cde4e3c66b7452be62f10dbc03bced9
pcsmomo <dongwoo_333@hanmail.net>
2021-06-30 18:54:41 +1000

    80. Disable Order Button if No Scoops Ordered

--- sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -3,12 +3,13 @@ import {
   screen,
   waitFor
 } from '../../../test-utils/testing-library-utils';
+import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
 
 import OrderEntry from '../OrderEntry';
 
-test.only('handles error for scoops and toppings routes', async () => {
+test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
       res(ctx.status(500))
@@ -27,4 +28,23 @@ test.only('handles error for scoops and toppings routes', async () => {
   });
 });
 
-// test('not a real test', () => {});
+test('disable order button if there are no scoops ordered', async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  // order button should be disabled at first, even before options load
+  const orderButton = screen.getByRole('button', { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  // expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla'
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1');
+  expect(orderButton).toBeEnabled();
+
+  // expect button to be disabled after removing scoop
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '0');
+  expect(orderButton).toBeDisabled();
+});
